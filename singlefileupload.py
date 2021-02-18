f2fa0ed3cb6d9ef8e3990a81a658e8766967a6f7
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-18 19:41:27 +0000

    AAH2-3 #1 - It works! Finished!

--- singlefileupload.py
+++ singlefileupload.py
@@ -1,20 +1,54 @@
-import os  # For File Manipulations like get paths, rename
+import os
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.secret_key = "secret key" 
+app=Flask(__name__)
 
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # maximum file size is 16mb
+app.secret_key = "secret key"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-path = os.getcwd() # returns current working directory of a process.
+path = os.getcwd()
+# file Upload
 UPLOAD_FOLDER = os.path.join(path, 'uploads')
-if not os.path.isdir(UPLOAD_FOLDER): # if the folder defined in "UPLOAD_FOLDER" does not exist...
-    os.mkdir(UPLOAD_FOLDER) # create a folder with the name defined in "UPLOAD_FOLDER"
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']) # only image files, pdfs and txt files can be uploaded according to these values
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run(host = '127.0.0.1',port = 5000, debug = False)
