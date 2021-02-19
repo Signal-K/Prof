743eea35f739b65b8a8ab372e66613cb12b686f5
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-19 05:16:24 +0000

    AAH2-3 #1 #2 #3 #4 AAH2-6 AAH2-7 Finish login and file upload

--- loginapp/main.py
+++ loginapp/main.py
@@ -1,11 +1,57 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Flask, request, redirect, flash
 from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
+import os
 
+app = Flask(__name__)
 main = Blueprint('main', __name__)
+
+app.secret_key = "secret key"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+path = os.getcwd()
+# file Upload
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/uploadme')
+def upload_form():
+    return render_template('upload.html')
+
+@main.route('/uploadme', methods=['POST'])
+def uploadme():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/login')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
 
 @main.route('/profile')
 @login_required
