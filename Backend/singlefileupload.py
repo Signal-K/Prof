c39396edc8cab54dd25f17e60385c9aa947dab0f
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-19 02:51:46 +0000

    AAH2-6: Finish login app

--- Backend/singlefileupload.py
+++ Backend/singlefileupload.py
@@ -49,6 +49,5 @@ def upload_file():
             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
             return redirect(request.url)
 
-
 if __name__ == "__main__":
     app.run(host = '127.0.0.1',port = 5000, debug = False)
