9e5e9ab5cdea6100abb96d407eebef934f29ae99
Gizmotronn <droidology@allianceofdroids.org.au>
2022-05-25 20:55:40 +0800

    🗝🧪 -> Hardhat artifacts for #20 SSDROP-3

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,8 +22,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_RINKEBY_RPC_URL, // the infura rinkeby url
+      accounts: [process.env.REACT_APP_PRIVATE_KEY] // metamask private key
+    },
     hardhat: {
       chainId: 1337,
     },
   },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY, // Your etherscan api key
+  }
 };
